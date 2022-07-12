b3456856d6d6406827cc89ad5c1def6b77d9405e
TofuStore <humormy@gmail.com>
2022-07-12 10:12:27 -0700

    FizzBuzz completed

--- src/main/java/com/galvanize/Application.java
+++ src/main/java/com/galvanize/Application.java
@@ -3,7 +3,25 @@ package com.galvanize;
 public class Application {
 
     public static void main(String[] args) {
-        System.out.println("hello world");
+        if (args.length == 0) {
+            System.out.println("Enter a number");
+        } else {
+            int i = Integer.parseInt(args[0]);
+            String output = "";
+
+            if (i % 3 == 0) {
+                output += "Fizz";
+            }
+            if (i % 5 == 0) {
+                output += "Buzz";
+            }
+
+            if (output.length() == 0) {
+                System.out.println(i);
+            } else {
+                System.out.println(output);
+            }
+        }
     }
 
 }
